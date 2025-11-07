b5c41d74685c91dde8eb968ba0febbbd0a8915b0
swordcube <swordcube@noreply.localhost>
2025-11-07 17:52:54 -0600

    use core opengl on linux

--- project/src/backend/sdl/SDLWindow.cpp
+++ project/src/backend/sdl/SDLWindow.cpp
@@ -104,7 +104,7 @@ namespace lime {
 			#endif
 
 			#if defined (HX_LINUX)
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+			SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
 			#endif
